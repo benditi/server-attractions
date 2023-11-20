79638f1894cd5cc72d4dc6559efabadabf346f5c
benditi <benditi@gmail.com>
2023-11-20 17:30:07 +0200

    Added validations and global error handleing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import taskRoutes from './routes/tasks'
+import cors from 'cors'
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors())
 app.use('/tasks', taskRoutes)
-
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
+  });
+
+  // Add this error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction):void => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong');
   });
 
   app.listen(port, () => {
--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,18 +1,32 @@
 import { Router, Request, Response } from "express";
 import { Task } from "../models/task";
+import { body, validationResult } from "express-validator";
 import fs from "fs";
 
 const router = Router();
 let tasks: Task[] = [];
 
+const taskValidationRules = [
+  body("title").notEmpty().withMessage("Title is required"),
+  body("description").notEmpty().withMessage("Description is required"),
+  body("completed").isBoolean().withMessage("Completed must be a boolean"),
+];
+
 // Add your CRUD API implementation here
 
-router.post("/", (req: Request, res: Response) => {
+router.post("/", taskValidationRules, (req: Request, res: Response) => {
+    console.log('@@req.body',req.body);
+    console.log('@@req.headers',req.headers);
+    
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const task: Task = {
     id: tasks.length + 1,
     title: req.body.title,
     description: req.body.description,
-    completed: false,
+    completed: req.body.completed,
   };
   tasks.push(task);
   res.status(201).json(task);
@@ -31,7 +45,12 @@ router.get("/:id", (req: Request, res: Response) => {
   }
 });
 
-router.put("/:id", (req: Request, res: Response) => {
+router.put("/:id", taskValidationRules, (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   let task = tasks.find((t) => t.id === parseInt(req.params.id));
   if (!task) {
     res.status(404).json("Task not found");
@@ -48,7 +67,7 @@ router.delete("/:id", (req: Request, res: Response) => {
   if (taskIdx === -1) {
     res.status(404).json("Task not found");
   } else {
-    tasks.splice(taskIdx,1);
+    tasks.splice(taskIdx, 1);
     res.status(204).send();
   }
 });
