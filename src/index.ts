33f70c6348e137d700f271e3231ae68c3e822072
benditi <benditi@gmail.com>
2023-12-17 15:36:30 +0200

    Changed app.use cors

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,18 @@ import path from "path";
 const app = express();
 const port = process.env.PORT || 3030;
 app.use(express.json());
-if (process.env.NODE_ENV==='production'){
-  app.use(express.static(path.resolve(__dirname, 'public')))
-} else {
   const corsOptions = {
     origin: [
       "http://127.0.0.1:8080",
       "http://localhost:8080",
       "http://127.0.0.1:3000",
       "http://localhost:3000",
+      "https://nextjs-blog-zeta-eosin-57.vercel.app/"
     ],
     credentials: true,
   };
   app.use(cors(corsOptions));
-}
+
 app.use("/api/attraction", attractionRoutes);
 
 // Add this error handling middleware
